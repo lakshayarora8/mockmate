6b4bbebdcb3b337799dd348c5b2dba7120a4a995
Lakshay Arora <a.lakshayjazz@gmail.com>
2024-07-15 03:00:01 +0530

    Gemini Setup complete

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,21 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import AddNewInterview from './_components/AddNewInterview'
+import InterviewList from './_components/InterviewList'
 
 function Dashboard() {
   return (
-    <div>Dashboard
+    <div className='p-10'>
 
-      <UserButton/>
+      <h2 className='font-bold text-3xl text-primary'>Dashboard</h2>
+      <h2 className='text-gray-500'>Create and Start your AI Mockup Interview</h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 my-5 gap-5'>
+        <AddNewInterview/>
+      </div>
+
+      {/* Previous Interview List  */}
+      <InterviewList/>
     </div>
   )
 }
