70a081086b030e8f28e550b95a223a876f2e6ae6
Lakshay Arora <a.lakshayjazz@gmail.com>
2024-07-19 04:52:25 +0530

    New logo

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
 
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
+        <Image src={'/newlogo.png'} width={160} height={100} alt='logo' />
         <ul className='hidden md:flex gap-6'>
           <Link href={"/dashboard"}>
             <li className={`hover:text-primary hover:font-bold transition-all
