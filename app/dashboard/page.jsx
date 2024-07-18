695f3c0f7d3da87fbc47562c136d532db1cde157
Lakshay Arora <a.lakshayjazz@gmail.com>
2024-07-19 02:05:59 +0530

    Questions section and Answer recording

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import AddNewInterview from './_components/AddNewInterview'
-import InterviewList from './_components/InterviewList'
+// import InterviewList from './_components/InterviewList'
 
 function Dashboard() {
   return (
@@ -15,7 +15,7 @@ function Dashboard() {
       </div>
 
       {/* Previous Interview List  */}
-      <InterviewList/>
+      {/* <InterviewList/> */}
     </div>
   )
 }
